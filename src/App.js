c12be801fc972d46999067fead2b948f2d8f359d
Prince Raj <39306689+Prince0000@users.noreply.github.com>
2024-08-24 19:48:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import HomePage from './components/HomePage';
 import {BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import ClickPage from './components/ClickPage';
 import Boost from './components/Boost';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('prince');
+  useEffect(() => {
+    // Retrieve Telegram username and log it to console
+    if (window.Telegram?.WebApp) {
+        const tg = window.Telegram.WebApp;
+        const user = tg.initDataUnsafe?.user;
+
+        if (user) {
+            setUsername(user.username);
+            console.log(`Telegram Username: ${user.username}`);
+        } else {
+          setUsername('prince17');
+            console.log('No Telegram user detected');
+        }
+    }
+}, []);
+
   return (
     <>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} >
-          <Route path='' element={<ClickPage/>}/>
-          <Route path='/boost' element={<Boost/>}/>
+          <Route path='' element={<ClickPage user={username}/>}/>
+          <Route path='/boost' element={<Boost user={username}/>}/>
         </Route>
       </Routes>
     </Router>
