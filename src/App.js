02de27eadf4b2e8206da36d5d62eb13cf32dd817
Prince Raj <39306689+Prince0000@users.noreply.github.com>
2024-08-24 19:58:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
-import {BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ClickPage from './components/ClickPage';
 import Boost from './components/Boost';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [username, setUsername] = useState('prince');
+  const [username, setUsername] = useState('');
+
   useEffect(() => {
     // Retrieve Telegram username and log it to console
     if (window.Telegram?.WebApp) {
-        const tg = window.Telegram.WebApp;
-        const user = tg.initDataUnsafe?.user;
+      const tg = window.Telegram.WebApp;
+      const user = tg.initDataUnsafe?.user;
 
-        if (user) {
-            setUsername(user.username);
-            console.log(`Telegram Username: ${user.username}`);
-        } else {
-          setUsername('prince17');
-            console.log('No Telegram user detected');
-        }
+      if (user?.username) {
+        setUsername(user.username);
+        console.log(`Telegram Username: ${user.username}`);
+      } else {
+        setUsername(null); // Set to null if no username is found
+        console.log('No Telegram username detected');
+      }
     }
-}, []);
+  }, []);
+
+  if (!username) {
+    return (
+      <div className="container-fluid bg-dark  d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
+        <header className="text-white">
+          <p>It looks like you don't have a username set in Telegram.</p>
+          <p>To use this app, please set a username in your Telegram settings:</p>
+          <ol>
+            <li>Open Telegram and go to "Settings".</li>
+            <li>Tap on "Username".</li>
+            <li>Set your desired username.</li>
+          </ol>
+          <p>After setting a username, refresh this page.</p>
+        </header>
+      </div>
+    );
+  }
 
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} >
-          <Route path='' element={<ClickPage user={username}/>}/>
-          <Route path='/boost' element={<Boost user={username}/>}/>
-        </Route>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} >
+            <Route path='' element={<ClickPage user={username} />} />
+            <Route path='/boost' element={<Boost user={username} />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
